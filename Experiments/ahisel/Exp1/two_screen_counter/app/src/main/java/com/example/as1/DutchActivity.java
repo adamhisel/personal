bd9f978f9a2a10b45db94764af98237baaff0ba6
adam <ahisel@iastate.edu>
2023-09-07 16:19:52 -0500

    Second Commit - Exp1

--- Experiments/ahisel/Exp1/two_screen_counter/app/src/main/java/com/example/as1/DutchActivity.java
+++ Experiments/ahisel/Exp1/two_screen_counter/app/src/main/java/com/example/as1/DutchActivity.java
@@ -2,13 +2,30 @@ package com.example.as1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DutchActivity extends AppCompatActivity {
+
+    Button returnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dutch);
+
+        returnButton = (Button) findViewById(R.id.button);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DutchActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
--- Experiments/ahisel/Exp1/two_screen_counter/app/src/main/java/com/example/as1/FrenchActivity.java
+++ Experiments/ahisel/Exp1/two_screen_counter/app/src/main/java/com/example/as1/FrenchActivity.java
@@ -2,13 +2,28 @@ package com.example.as1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FrenchActivity extends AppCompatActivity {
+
+    Button returnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_french);
+
+        returnButton = (Button) findViewById(R.id.button);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(FrenchActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
