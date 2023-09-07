ea6126563952cec07f30340647169b3ef970034c
jpatel08 <jaypatel515121@gmail.com>
2023-09-07 12:23:36 -0500

    added variable to helloworld

--- Experiments/jpatel/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/jpatel/helloworld/src/main/java/coms309/WelcomeController.java
@@ -12,8 +12,8 @@ class WelcomeController {
         return "Hello and welcome to COMS 309";
     }
 
-    @GetMapping("/{name}")
-    public String welcome(@PathVariable String name) {
-        return "Your name is:  " + name;
+    @GetMapping("/{name}/{number}")
+    public String welcome(@PathVariable String name, @PathVariable String number) {
+        return ( "Your name is:  " + name + "Your age is: "+ number);
     }
 }
