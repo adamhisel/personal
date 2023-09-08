e668dd16ecf39da12adf7f4f17e364950416a498
gourley <gourley@iastate.edu>
2023-09-07 21:54:27 -0500

    Experiment to move between screens, displaying an int on the second screen that was entered on the first

--- Experiments/gourley/Exp3/Experiment3/app/src/main/java/com/example/experiment3/MainActivity.java
+++ Experiments/gourley/Exp3/Experiment3/app/src/main/java/com/example/experiment3/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.experiment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNumber;
+    Button btnSecondScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNumber = findViewById(R.id.etNumber);
+        btnSecondScreen = findViewById(R.id.btnSecondScreen);
+
+        btnSecondScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int number = Integer.parseInt(etNumber.getText().toString());
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("EXTRA_NUMBER", number);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- Experiments/gourley/Exp3/Experiment3/app/src/main/java/com/example/experiment3/MainActivity2.java
+++ Experiments/gourley/Exp3/Experiment3/app/src/main/java/com/example/experiment3/MainActivity2.java
@@ -2,13 +2,34 @@ package com.example.experiment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    TextView tvNumber;
+    Button btnGoBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        int number = getIntent().getIntExtra("EXTRA_NUMBER", 0);
+
+        tvNumber = findViewById(R.id.tvNumber);
+        btnGoBack = findViewById(R.id.btnGoBack);
+
+        tvNumber.setText(String.valueOf(number));
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
