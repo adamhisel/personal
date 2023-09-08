4da8ce02297b1c0ee78b43fe9fd951d8560b513d
adam <ahisel@iastate.edu>
2023-09-08 14:40:44 -0500

    Final Commit Updated

--- Experiments/ahisel/Exp3/app/src/main/java/com/example/madeshotadder/ShootingPercentage.java
+++ Experiments/ahisel/Exp3/app/src/main/java/com/example/madeshotadder/ShootingPercentage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ShootingPercentage extends AppCompatActivity {
@@ -15,17 +17,33 @@ public class ShootingPercentage extends AppCompatActivity {
 
     double total;
 
+    Button b;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shooting_percentage);
         Intent i = getIntent();
         shootingPercent = findViewById(R.id.percent);
+        b = findViewById(R.id.button);
 
         made = i.getIntExtra("madeNum", 0);
         miss = i.getIntExtra("missNum",0);
-        total = (made/miss) * 100;
+        total = (made/(miss+made)) * 100;
         shootingPercent.setText(String.valueOf(total) + "%");
+
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+
+                Intent i = new Intent(ShootingPercentage.this, MainActivity.class);
+                startActivity(i);
+            }
+
+
+        });
 
 
     }
