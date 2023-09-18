8b2120cf74e860a9f12a9d0f66f68ac233b1b1b1
gourley <gourley@iastate.edu>
2023-09-18 10:49:23 -0500

    Beginning experiment for nav bar

---
+++ Experiments/gourley/Exp_nav_bar/ExpNavBar/app/src/main/java/com/example/expnavbar/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.expnavbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main);
+    }
+}
