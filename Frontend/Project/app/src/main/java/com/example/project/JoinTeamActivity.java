1d694627058ce02d63c25b71a1e39b9e41a9350f
adam <ahisel@iastate.edu>
2023-10-30 12:13:08 -0500

    Implmenting join method

--- Frontend/Project/app/src/main/java/com/example/project/JoinTeamActivity.java
+++ Frontend/Project/app/src/main/java/com/example/project/JoinTeamActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,6 +19,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -31,6 +33,9 @@ public class JoinTeamActivity extends AppCompatActivity {
     private RequestQueue mQueue;
     private ArrayList<String> teamArr;
 
+    private ArrayList<String> idArr;
+
+    private int teamId;
     private AutoCompleteTextView teamNameAutoComplete;
     private AutoCompleteTextView typeAutoComplete;
 
@@ -52,6 +57,7 @@ public class JoinTeamActivity extends AppCompatActivity {
         typeArr[1] = "Fan";
 
         fillTeamList(new TeamListCallback() {
+
             @Override
             public void onTeamListReceived(ArrayList<String> teamList) {
                 teamArr = teamList;
@@ -62,18 +68,31 @@ public class JoinTeamActivity extends AppCompatActivity {
             }
         });
 
+        fillIdList(new TeamListCallback() {
+            @Override
+            public void onTeamListReceived(ArrayList<String> teamList) {
+                idArr = teamList;
+            }
+        });
+
+        teamNameAutoComplete.setOnItemClickListener((parent, view, position, id) -> {
+            String selected = (String) parent.getItemAtPosition(position);
+            teamId = Integer.parseInt(idArr.get(position));
+        });
+
+
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(JoinTeamActivity.this, android.R.layout.simple_dropdown_item_1line, typeArr);
         typeAutoComplete.setAdapter(adapter2);
 
 
         typeAutoComplete.setOnItemClickListener((parent, view, position, id) -> {
-            String selectedSuggestion2 = (String) parent.getItemAtPosition(position);
+            String selected = (String) parent.getItemAtPosition(position);
+
         });
 
         joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(JoinTeamActivity.this, MainActivity.class);
                 startActivity(intent);
             }
@@ -93,9 +112,39 @@ public class JoinTeamActivity extends AppCompatActivity {
                             for (int i = 0; i < response.length(); i++) {
                                 JSONObject team = response.getJSONObject(i);
                                 String name = team.getString("teamName");
-                                String id = team.getString("id");
+                                //teamId = team.getInt("id");
                                 temp1.add(name);
 
+                            }
+
+                            callback.onTeamListReceived(temp1);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+
+    private void fillIdList(final TeamListCallback callback) {
+        String url = "http://10.0.2.2:8080/teams";
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            ArrayList<String> temp1 = new ArrayList<>();
+
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject team = response.getJSONObject(i);
+                                String id = team.getString("id");
+                                temp1.add(id);
                             }
 
                             callback.onTeamListReceived(temp1);
@@ -118,13 +167,56 @@ public class JoinTeamActivity extends AppCompatActivity {
     }
 
 
-    private void joinTeam(){
+    private void joinTeamUser(){
+        String userId = SharedPrefsUtil.getUserId(this);
+
+        String url = "http://coms-309-018.class.las.iastate.edu:8080/User/" + userId + "/teams/" + teamId;
+
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+                response -> {
+                    if ("success".equals(response)) {
+
+                    } else if ("failure".equals(response)) {
+                    }
+                },
+                error -> {
+
+                    if (error.networkResponse != null) {
+                        String errorMessage = new String(error.networkResponse.data);
+                        Log.e("JoinTeam", "Error in request: " + errorMessage);
+                    }
+                }
+        );
+
+        mQueue.add(putRequest);
 
 
     }
 
+    private void joinTeamPlayer(){
+        String userId = SharedPrefsUtil.getUserId(this);
+
+        String url = "http://coms-309-018.class.las.iastate.edu:8080/User/" + userId + "/teams/" + teamId;
+
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+                response -> {
+                    if ("success".equals(response)) {
+
+                    } else if ("failure".equals(response)) {
+                    }
+                },
+                error -> {
+
+                    if (error.networkResponse != null) {
+                        String errorMessage = new String(error.networkResponse.data);
+                        Log.e("JoinTeam", "Error in request: " + errorMessage);
+                    }
+                }
+        );
+
+        mQueue.add(putRequest);
 
 
-
+    }
 
 }
