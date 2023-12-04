d4f78b6acd18f57dcd510d5c476cfcceb92d5617
adam <ahisel@iastate.edu>
2023-12-04 01:55:22 -0600

    Almost done with games on home page

--- Frontend/Project/app/src/main/java/com/example/project/HomeFragment.java
+++ Frontend/Project/app/src/main/java/com/example/project/HomeFragment.java
@@ -6,10 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -28,6 +32,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Home fragment in the navigation bar that contains options to create a team, join a team
@@ -42,6 +49,23 @@ public class HomeFragment extends Fragment {
     private LinearLayout ll;
 
     private Bundle savedInstance;
+
+    private Map<ToggleButton, LinearLayout> toggleButtonMap = new HashMap<>();
+
+    private int totalPoints = 0;
+    private int totalGames = 0;
+    private int totalFGM = 0;
+    private int totalFGA = 0;
+    private int total3PM = 0;
+    private int total3PA = 0;
+    private int total2PM = 0;
+    private int total2PA = 0;
+
+    private ArrayList<Integer> gameIdArr;
+
+    private ArrayList<Player> players;
+
+    private Map<Integer, Integer> playerPointsMap = new HashMap<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -52,15 +76,15 @@ public class HomeFragment extends Fragment {
 
         mQueue = Volley.newRequestQueue(requireContext());
 
-        ll = view.findViewById(R.id.linearLayout);
+        ll = view.findViewById(R.id.cardLL);
 
         ImageButton addTeam = view.findViewById(R.id.plus);
 
         ImageButton joinTeam = view.findViewById(R.id.join);
 
-        //TextView header = view.findViewById(R.id.header);
+        TextView welcome = view.findViewById(R.id.welcome);
 
-        //header.setText("Hello, " + SharedPrefsUtil.getFirstName(requireContext()));
+        welcome.setText("Hello, " + SharedPrefsUtil.getFirstName(requireContext()));
 
         displayTeamButtons();
         addTeam.setOnClickListener(new View.OnClickListener() {
@@ -123,15 +147,129 @@ public class HomeFragment extends Fragment {
 
                         for(int j =0; j< teamList.size(); j++) {
 
-                            Button button = new Button(requireContext());
+                            totalPoints = 0;
+                            totalGames = 0;
+                            totalFGM = 0;
+                            totalFGA = 0;
+                            total3PM = 0;
+                            total3PA = 0;
+                            total2PM = 0;
+                            total2PA = 0;
+
+                            CardView cv = new CardView(requireContext());
+
+                            LinearLayout.LayoutParams cardParams = new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.WRAP_CONTENT
+                            );
+                            cardParams.setMargins(8, 8, 8, 8);
+                            cv.setLayoutParams(cardParams);
+                            cv.setCardElevation(4);
+
+                            LinearLayout linearLayout = new LinearLayout(requireContext());
+                            linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.WRAP_CONTENT
+                            ));
+                            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
 
                             String teamName = teamList.get(j);
                             int id = teamIds.get(j);
 
-                            button.setText(teamName);
+                            cv.setId(id);
+
+                            LinearLayout.LayoutParams halfLLHor = new LinearLayout.LayoutParams(
+                                    0,
+                                    LinearLayout.LayoutParams.MATCH_PARENT
+                            );
+
+                            LinearLayout.LayoutParams halfLLVert = new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    0
+                            );
+
+                            halfLLHor.weight = 1;
+                            halfLLVert.weight = 1;
+
+                            TextView tv = new TextView(requireContext());
+
+                            tv.setLayoutParams(halfLLHor);
+                            tv.setText(teamName);
+                            tv.setTextSize(25);
+
+
+
+                            LinearLayout buttonLayout = new LinearLayout(requireContext());
+
+                            buttonLayout.setOrientation(LinearLayout.VERTICAL);
+
+                            buttonLayout.setLayoutParams(halfLLHor);
+
+
+
+                            Button button = new Button(requireContext());
+
+                            button.setLayoutParams(halfLLVert);
+                            button.setText("View Team");
                             button.setTag(id);
                             button.setTextSize(25);
 
+                            ToggleButton tButton = new ToggleButton(requireContext());
+
+                            tButton.setLayoutParams(halfLLVert);
+                            tButton.setText("View Recent Games");
+                            tButton.setTextOff("View Recent Games");
+                            tButton.setTextOn("Hide Recent Games");
+                            tButton.setTag(id);
+                            tButton.setTextSize(15);
+
+
+
+                            buttonLayout.addView(button);
+                            buttonLayout.addView(tButton);
+
+                            linearLayout.addView(tv);
+                            linearLayout.addView(buttonLayout);
+
+                            LinearLayout gamesLL = new LinearLayout(requireContext());
+                            gamesLL.setLayoutParams(new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.WRAP_CONTENT
+                            ));
+                            gamesLL.setOrientation(LinearLayout.HORIZONTAL);
+                            gamesLL.setVisibility(View.GONE);
+
+                            toggleButtonMap.put(tButton, gamesLL);
+
+                            LinearLayout cardContentLayout = new LinearLayout(requireContext());
+                            cardContentLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.WRAP_CONTENT
+                            ));
+                            cardContentLayout.setOrientation(LinearLayout.VERTICAL);
+
+
+                            cardContentLayout.addView(linearLayout);
+                            cardContentLayout.addView(gamesLL);
+
+
+
+                            cv.addView(cardContentLayout);
+
+
+                            tButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                                @Override
+                                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                    LinearLayout gamesLL = toggleButtonMap.get(buttonView);
+                                    if (isChecked) {
+                                        gamesLL.removeAllViews();
+                                        getGames(id, gamesLL);
+                                    } else {
+                                        gamesLL.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
                             button.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View view) {
@@ -161,10 +299,11 @@ public class HomeFragment extends Fragment {
                                         }
 
                                         SharedPrefsTeamUtil.saveTeamData(getContext(), teamName, String.valueOf(id), isCoach, isFan);
-                                        TeamRosterFragment fragment = new TeamRosterFragment();
                                         FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                                        TeamRosterFragment newFragment = new TeamRosterFragment();
+
                                         FragmentTransaction transaction = fragmentManager.beginTransaction();
-                                        transaction.replace(R.id.linear, fragment);
+                                        transaction.replace(R.id.linear, newFragment);
                                         transaction.addToBackStack(null);
                                         transaction.commit();
 
@@ -172,7 +311,7 @@ public class HomeFragment extends Fragment {
                                 }
                             });
 
-                            ll.addView(button, ll.getChildCount() - teams.length()-1);
+                            ll.addView(cv, ll.getChildCount() - teams.length()-2);
 
                         }
 
@@ -190,5 +329,102 @@ public class HomeFragment extends Fragment {
         mQueue.add(request);
     }
 
+
+    private void getGames(int id, LinearLayout gamesLL) {
+        String url = "http://coms-309-018.class.las.iastate.edu:8080/games/" + id;
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject game = response.getJSONObject(i);
+                            int gameId = game.getInt("id");
+                            String gameDate = game.getString("date");
+
+                            // Calculate statistics for this game
+                            calculateGameStatistics(gameId, gamesLL, gameDate);
+                        }
+
+                        // Show the gamesLL layout with all the game statistics
+                        gamesLL.setVisibility(View.VISIBLE);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> error.printStackTrace()
+        );
+        mQueue.add(request);
+    }
+
+    private void calculateGameStatistics(int gameId, LinearLayout gamesLL, String gameDate) {
+        String url = "http://coms-309-018.class.las.iastate.edu:8080/games/" + gameId + "/shots";
+        JsonArrayRequest shotsRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    int gameFGM = 0;
+                    int gameFGA = 0;
+                    int game3PM = 0;
+                    int game3PA = 0;
+
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject shot = response.getJSONObject(i);
+                            boolean made = shot.getBoolean("made");
+                            int value = shot.getInt("value");
+
+                            // Update game statistics
+                            if (made) {
+                                gameFGM++;
+                            }
+                            gameFGA++;
+
+                            if (value == 3) {
+                                if (made) {
+                                    game3PM++;
+                                }
+                                game3PA++;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    // Calculate shooting percentages for this game
+                    double gameFGPercentage = gameFGA > 0 ? (gameFGM * 100.0 / gameFGA) : 0.0;
+                    double game3PPercentage = game3PA > 0 ? (game3PM * 100.0 / game3PA) : 0.0;
+
+                    // Display game statistics in the layout
+                    displayGameStatistics(gamesLL, gameDate, gameFGPercentage, game3PPercentage);
+                },
+                error -> error.printStackTrace()
+        );
+        mQueue.add(shotsRequest);
+    }
+
+    private void displayGameStatistics(LinearLayout gamesLL, String gameDate, double gameFGPercentage, double game3PPercentage) {
+        LinearLayout gameLayout = new LinearLayout(requireContext());
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        gameLayout.setLayoutParams(layoutParams);
+        gameLayout.setOrientation(LinearLayout.VERTICAL);
+
+        TextView date = new TextView(requireContext());
+        date.setText("Date: " + gameDate);
+        date.setTextSize(20);
+
+        TextView fgP = new TextView(requireContext());
+        fgP.setText(String.format(Locale.US, "FG%%: %.2f%%", gameFGPercentage));
+        fgP.setTextSize(20);
+
+        TextView threeP = new TextView(requireContext());
+        threeP.setText(String.format(Locale.US, "3PT%%: %.2f%%", game3PPercentage));
+        threeP.setTextSize(20);
+
+        gameLayout.addView(date);
+        gameLayout.addView(fgP);
+        gameLayout.addView(threeP);
+
+        gamesLL.addView(gameLayout);
+    }
 
 }
