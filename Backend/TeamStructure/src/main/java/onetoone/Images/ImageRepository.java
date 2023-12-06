510047a5754023340ac802589e0c0822de74e231
jpatel08 <jaypatel515121@gmail.com>
2023-12-06 17:06:30 -0600

    added team and userId methods for images

--- Backend/TeamStructure/src/main/java/onetoone/Images/ImageRepository.java
+++ Backend/TeamStructure/src/main/java/onetoone/Images/ImageRepository.java
@@ -3,7 +3,12 @@ package onetoone.Images;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ImageRepository extends JpaRepository<Image,Integer> {
 
+    Optional<Image> findByUserId(int userId);
+
+    Optional<Image> findByTeamId(int teamId);
 }
